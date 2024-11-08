6d67bdb95b5720531c5d54a76066d74b1cf8eb4d
Antonio Colagreco <92807691+antoniocolagreco@users.noreply.github.com>
2024-11-08 16:23:27 +0100

    chore: added tailwind and other astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind(), react(), mdx(), sitemap()],
+})
